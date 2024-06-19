e76283bc3b4bea3d6fcb6af9ff449afbc975577e
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-19 17:21:26 +0530

    Update

--- myproj4/myapp/models.py
+++ myproj4/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.TextField(max_length=200)
+    age = models.IntegerField()
+    mobile = models.CharField(max_length=10)
+    dob = models.DateField()
+
--- myproj4/myapp/views.py
+++ myproj4/myapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import Student
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        user_name = request.POST['uname']
+        user_email = request.POST['uemail']
+        user_age = request.POST['uage']
+        user_address = request.POST['address']
+        user_mobile = request.POST['mobile']
+        user_dob = request.POST['dob']
+        Student(name=user_name,email=user_email,age=user_age,address=user_address,mobile=user_mobile,dob=user_dob).save()
+        return HttpResponse('Data Saved !!')
+       
     return render(request,'Form.html')
 
 def Table(request):
